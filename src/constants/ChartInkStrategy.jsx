19bc9716e05a1971355ebaa3bf8e8d706fcec86d
imshahbaz21 <imshahbaz21@gmail.com>
2025-02-27 04:13:03 +0530

    Vollinger

--- src/constants/ChartInkStrategy.jsx
+++ src/constants/ChartInkStrategy.jsx
@@ -1,53 +1,8 @@
 export const chartink_strategy = [
   {
-    name: 'RSE',
-    link: 'https://chartink.com/screener/crossing-5011',
-    display: false,
-  },
-  {
-    name: 'Shiffon',
-    link: 'https://chartink.com/screener/s4-1147',
-    display: false,
-  },
-  {
-    name: 'Hammer',
-    link: 'https://chartink.com/screener/hammer-16123355',
-    display: false,
-  },
-  {
-    name: '3:30 Formula',
-    link: 'https://chartink.com/screener/3-30-2138',
-    display: false,
-  },
-  {
-    name: 'Bullish Harami',
-    link: 'https://chartink.com/screener/bullish-harami-9503969778',
-    display: false,
-  },
-  {
-    name: '3 Inside Up',
-    link: 'https://chartink.com/screener/adx-cross-578',
-    display: false,
-  },
-  {
-    name: 'Super Bull',
-    link: 'https://chartink.com/screener/lorse',
-    display: false,
-  },
-  {
-    name: 'Rollinger',
-    link: 'https://chartink.com/screener/vs-591',
-    display: false,
-  },
-  {
-    name: 'Super Bollinger',
-    link: 'https://chartink.com/screener/superbollinger-2',
-    display: false,
-  },
-  {
-    name: '3:30 Intraday',
-    link: 'https://chartink.com/screener/3-30-intraday-8',
-    display: false,
+    name: 'Vollinger',
+    link: 'https://chartink.com/screener/vollinger',
+    display: true,
   },
   {
     name: '20:50',
